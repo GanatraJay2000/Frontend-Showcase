442939682e68972a08e048615bdaf39200c01aff
Jay Ganatra <ganatrajay2000@gmail.com>
2024-08-02 12:41:50 -0400

    build fix

--- src/pages/[pen]/Item.tsx
+++ src/pages/[pen]/Item.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import Script from 'next/script'
 import { penBaseURL } from "@/lib/itemMap";
-
 
 function Item({ pen }: { pen?: string }) {
   const penURL = penBaseURL + pen;
   return (
     <div className="w-full flex h-full bg-gray-600">
-      <Script strategy="lazyOnload" async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></Script>
       <iframe  
         style={{"width": "100%",
           "height": "100%",
@@ -16,8 +13,6 @@ function Item({ pen }: { pen?: string }) {
         loading="lazy" 
         allowFullScreen
       />
-      
-
     </div>
   );
 }
